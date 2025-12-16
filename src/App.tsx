0fbad25f624e8816e774bfff9a8b668420eae35b
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-16 19:38:44 +0900

    tailwindcss & plugins install

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-    return (
-        <div>Hello</div>
-    )
+  return <div className="bg-black text-2xl font-bold underline">Hello</div>;
 }
 
-export default App
+export default App;
