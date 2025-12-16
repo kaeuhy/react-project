ebc9062fae5343db853f5bff14532dd1f6c52b88
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-16 20:50:46 +0900

    feature: ui components - input.tsx - dialog.tsx - sonner.tsx - button.tsx - popover.tsx - textarea.tsx - carousel.tsx - alert-dialog.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
 import "./App.css";
 import { Button } from "@/components/ui/button.tsx";
+import { Toaster } from "@/components/ui/sonner.tsx";
+import { toast } from "sonner";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel.tsx";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover.tsx";
+import { ChefHat } from "lucide-react";
 
 function App() {
   return (
     <div>
-      <Button className="bg-yellow-500">버튼</Button>
+      <ChefHat />
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button>open</Button>
+        </PopoverTrigger>
+        <PopoverContent>content</PopoverContent>
+      </Popover>
+      <Toaster />
+      <Carousel className="mx-10">
+        <CarouselContent>
+          <CarouselItem>1</CarouselItem>
+          <CarouselItem>2</CarouselItem>
+          <CarouselItem>3</CarouselItem>
+        </CarouselContent>
+        <CarouselNext />
+        <CarouselPrevious />
+      </Carousel>
+      <Button
+        onClick={() => toast("안녕하세요", { position: "top-center" })}
+        className="bg-yellow-500"
+      >
+        버튼
+      </Button>
     </div>
   );
 }
