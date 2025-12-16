476f85550b9dcad7fe30dbc9309476dbcf605156
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-16 21:06:58 +0900

    React Router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,28 @@
 import "./App.css";
-import { Button } from "@/components/ui/button.tsx";
-import { Toaster } from "@/components/ui/sonner.tsx";
-import { toast } from "sonner";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel.tsx";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover.tsx";
-import { ChefHat } from "lucide-react";
+import { Outlet, Route, Routes } from "react-router";
+import IndexPage from "@/pages/index-page.tsx";
+import SignInPage from "@/pages/sign-in-page.tsx";
+import SignUpPage from "@/pages/sign-up-page.tsx";
+
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <ChefHat />
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button>open</Button>
-        </PopoverTrigger>
-        <PopoverContent>content</PopoverContent>
-      </Popover>
-      <Toaster />
-      <Carousel className="mx-10">
-        <CarouselContent>
-          <CarouselItem>1</CarouselItem>
-          <CarouselItem>2</CarouselItem>
-          <CarouselItem>3</CarouselItem>
-        </CarouselContent>
-        <CarouselNext />
-        <CarouselPrevious />
-      </Carousel>
-      <Button
-        onClick={() => toast("안녕하세요", { position: "top-center" })}
-        className="bg-yellow-500"
-      >
-        버튼
-      </Button>
-    </div>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 
