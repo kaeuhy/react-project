3c91e8f4208d1a33f0830064c4c065eef1355f96
kaeuhy <kaeuhy_0315@naver.com>
2026-01-25 21:43:40 +0900

    QueryKey factory 패턴을 사용한 데이터 무효화

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,7 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createTodo } from "@/api/create-todo.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
 export function useCreateTodoMutation() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createTodo,
     // createTodo 비동기 요청이 성공, 실패, 시작, 끝났을 시점에 원하는 동작을 수행하는 이벤트 핸들러들
@@ -11,7 +14,11 @@ export function useCreateTodoMutation() {
     onSettled: () => {},
     // 요청이 성공
     onSuccess: () => {
-      window.location.reload();
+      // quertClient는 tanstack query의 store라고 생각
+      // 해당 store에서 todos를 invalidateQueries를 통해 무효화
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list,
+      });
     },
     // 요청이 실패
     onError: (error) => {
