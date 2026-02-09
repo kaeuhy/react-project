95e1453d9775ece02b4fd55918e3cf0a81dd4b0d
kaeuhy <kaeuhy_0315@naver.com>
2026-02-09 16:52:17 +0900

    이미지 업로드 구현

--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -1,10 +1,16 @@
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import { ImageIcon } from "lucide-react";
+import { XIcon, ImageIcon } from "lucide-react";
 import { usePostEditorModal } from "@/store/post-editor-modal.ts";
-import { useEffect, useRef, useState } from "react";
+import { type ChangeEvent, useEffect, useRef, useState } from "react";
 import { useCreatePost } from "@/hooks/mutations/post/use-create-post.ts";
 import { toast } from "sonner";
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel.tsx";
+
+type Image = {
+  file: File,
+  previewUrl: string
+}
 
 export default function PostEditorModal() {
   const { isOpen, close } = usePostEditorModal();
@@ -20,7 +26,10 @@ export default function PostEditorModal() {
   });
 
   const [content, setContent] = useState("");
+  const [images, setImages] = useState<Image[]>([]);
+
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleCloseModal = () => {
     close();
@@ -29,6 +38,22 @@ export default function PostEditorModal() {
   const handleCreatePostClick = () => {
     if (content.trim() === "") return;
     createPost(content);
+  };
+
+  const handleSelectImages = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const files = Array.from(e.target.files);
+
+      files.forEach((file) => {
+        setImages((prev) => [...prev, { file, previewUrl: URL.createObjectURL(file) }]);
+      });
+    }
+
+    e.target.value = "";
+  };
+
+  const handleDeleteImage = (image: Image) => {
+    setImages((prevImages) => prevImages.filter((item) => item.previewUrl !== image.previewUrl));
   };
 
   useEffect(() => {
@@ -42,6 +67,7 @@ export default function PostEditorModal() {
     if (!isOpen) return;
     textareaRef.current?.focus();
     setContent("");
+    setImages([]);
   }, [isOpen]);
 
   return (
@@ -56,7 +82,27 @@ export default function PostEditorModal() {
           className="max-h-125 min-h-25 focus:outline-none"
           placeholder="무슨 일이 있었나요?"
         />
+        <input onChange={handleSelectImages} ref={fileInputRef} type="file" accept="image/*" multiple
+               className="hidden" />
+        {images.length > 0 && (<Carousel>
+            <CarouselContent>
+              {images.map((image) => (
+                <CarouselItem className="basis-2/5" key={image.previewUrl}>
+                  <div className="relative">
+                    <img src={image.previewUrl} className="h-full w-full rounded-sm object-cover" />
+                    <div onClick={() => handleDeleteImage(image)}
+                         className="absolute top-0 right-0 m-1 cursor-pointer rounded-full bg-black/30 p-1">
+                      <XIcon className="h-4 w-4 text-white" />
+                    </div>
+                  </div>
+                </CarouselItem>))}
+            </CarouselContent>
+          </Carousel>
+        )}
         <Button
+          onClick={() => {
+            fileInputRef.current?.click();
+          }}
           disabled={isCreatePostPending}
           variant={"outline"}
           className="cursor-pointer"
